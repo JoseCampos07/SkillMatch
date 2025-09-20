8991d789d1da441060caeeb4a59e9af8675769e3
JoseCampos07 <23151200@aguascalientes.tecnm.mx>
2025-09-20 09:31:22 -0600

    Adding Outgoing Payment

---
+++ transfering.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Command\OutgoingPayment;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+//@! start chunk 1 | title=Import dependencies
+use OpenPayments\AuthClient;
+use OpenPayments\Config\Config;
+//@! end chunk 1
+
+/**
+ * Class OutgoingPaymentCreateAmount
+ * @package App\Command\OutgoingPayment
+ *
+ * This command is used to create an outgoing payment with a specific amount.
+ * It outputs the outgoing payment object.
+ */
+
+class OutgoingPaymentCreateAmount extends Command
+{
+    protected static $defaultName = 'op:create:amount';
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('This command is used to create an outgoing payment with a specific amount.')
+            ->setHelp('This command outputs the outgoing payment object.')
+            ->addArgument(
+                'OUTGOING_PAYMENT_GRANT_ACCESS_TOKEN',
+                InputArgument::OPTIONAL,
+                'Access token for the outgoing payment received from the outgoing payment grant.',
+                $_ENV['OUTGOING_PAYMENT_GRANT_ACCESS_TOKEN'] ?? null
+            )
+            ->addArgument(
+                'INCOMING_PAYMENT_URL',
+                InputArgument::OPTIONAL,
+                'The url of the incoming payment.',
+                $_ENV['INCOMING_PAYMENT_URL']
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $WALLET_ADDRESS =  $_ENV['https://ilp.interledger-test.dev/e2c3b72c'];
+        $PRIVATE_KEY = $_ENV[<<<EOD
+        -----BEGIN PRIVATE KEY-----
+        MC4CAQAwBQYDK2VwBCIEINNa7A19IusDBYRxjaiwcchUFbdRZh4YndMcfFV8m9rd
+        -----END PRIVATE KEY-----
+        EOD];
+        $KEY_ID = $_ENV['abf39247-7cbd-4bb7-829e-9c634a03ed8b'];
+        $OUTGOING_PAYMENT_GRANT_ACCESS_TOKEN = $input->getArgument('OUTGOING_PAYMENT_GRANT_ACCESS_TOKEN');
+        $INCOMING_PAYMENT_URL = $input->getArgument('INCOMING_PAYMENT_URL');
+
+        //@! start chunk 2 | title=Initialize Open Payments client
+        $config = new Config(
+            $WALLET_ADDRESS, $PRIVATE_KEY, $KEY_ID
+        );
+        $opClient = new AuthClient($config);
+        //@! end chunk 2
+
+        $wallet  = $wallet = $opClient->walletAddress()->get([
+            'url' => $config->getWalletAddressUrl()
+        ]);
+
+        //@! start chunk 3 | title=Create outgoing payment with amount
+        $newOutgoingPayment = $opClient->outgoingPayment()->create(
+            [
+                'url' => $wallet->resourceServer,
+                'access_token' => $OUTGOING_PAYMENT_GRANT_ACCESS_TOKEN
+            ],
+            [
+                'walletAddress' => $config->getWalletAddressUrl(),
+                'incomingPayment' => $INCOMING_PAYMENT_URL,
+                'debitAmount' => [
+                    'value' => '121',
+                    'assetCode' => 'USD',
+                    'assetScale' => 2
+                ],
+                'metadata' => [
+                    'description' => 'Test outgoing payment',
+                    'reference' => '1234567890',
+                    'invoiceId' => '1234567890',
+                    'customData' => [
+                        'key1' => 'value1',
+                        'key2' => 'value2'
+                    ]
+                ],
+            ]
+        );
+        //@! end chunk 3
+
+        $output->writeln('OUTGOING_PAYMENT '.print_r($newOutgoingPayment, true));
+        //@! start chunk 4 | title=Output
+        $output->writeln('OUTGOING_PAYMENT_URL '.$newOutgoingPayment->id);
+        //@! end chunk 4
+
+        return Command::SUCCESS;
+    }
+}
+
+?>
